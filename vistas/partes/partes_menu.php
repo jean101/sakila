9d5d9121e56925b31480390c46578942da5f7bec
jean101 <jeancarlos0310@gmail.com>
2020-06-28 10:45:14 -0700

    añadiendo imagen

--- vistas/partes/partes_menu.php
+++ vistas/partes/partes_menu.php
@@ -1,5 +1,13 @@
 <nav class="nav flex-column">
 
+<nav class="navbar navbar-light bg-primary">
+  <div class="animate__backInDown delay-3s infinite">
+    <a class="navbar-brand" href="#">
+      <img src="http://freevector.co/wp-content/uploads/2013/03/2333-play-button1.png" width="30" height="30" class="d-inline-block align-top" alt="" loading="lazy">
+      sakila
+    </a>
+  </div>
+</nav>
   <?php
 
    $paginasMenu = [
@@ -30,6 +38,7 @@
 
  }
 
+ 
  ?>
 
  </nav>
